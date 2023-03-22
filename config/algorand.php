24f930da19db43d052ffcfcce86751419038ee28
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-03-22 11:47:20 +0100

    configuration

---
+++ config/algorand.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Algod Credentials
+    |--------------------------------------------------------------------------
+    |
+    */
+    'algod' => [
+        'api_url' => 'http://node.testnet.algoexplorerapi.io/',
+        'api_key' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+        'api_token_header' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Indexer Credentials
+    |--------------------------------------------------------------------------
+    |
+    */
+    'indexer' => [
+        'api_url' => 'http://node.testnet.algoexplorerapi.io/',
+        'api_key' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+        'api_token_header' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+    ],
+];
+
+
+
+// return [
+
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Algod Credentials
+//     |--------------------------------------------------------------------------
+//     |
+//     */
+//     'algod' => [
+//         'api_url' => 'https://testnet-algorand.api.purestake.io/ps2/v2/status',
+//         'api_key' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+//         'api_token_header' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+//     ],
+
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Indexer Credentials
+//     |--------------------------------------------------------------------------
+//     |
+//     */
+//     'indexer' => [
+//         'api_url' => 'https://testnet-algorand.api.purestake.io/idx2',
+//         'api_key' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+//         'api_token_header' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
+//     ],
+// ];
--- config/filesystems.php
+++ config/filesystems.php
@@ -17,19 +17,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Cloud Filesystem Disk
-    |--------------------------------------------------------------------------
-    |
-    | Many applications store files both locally and in the cloud. For this
-    | reason, you may specify a default "cloud" driver here. This driver
-    | will be bound as the Cloud disk implementation in the container.
-    |
-    */
-
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
-
-    /*
-    |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
     |
@@ -63,6 +50,11 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+        ],
+
+        'media' => [
+            'driver' => 'local',
+            'root'   => public_path('media'),
         ],
 
     ],
