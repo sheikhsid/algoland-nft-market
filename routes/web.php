35d60cb74d71c7ed49be6046b0f8e0b0e3a0e518
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-03-23 09:26:00 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\ExploreController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('welcome'); });
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/explore', [ExploreController::class, 'index'])->name('explore.index');
+
+Route::get('/collection', [CollectionController::class, 'index'])->name('collection.index');
+
+Route::get('/create/', [AssetController::class, 'create'])->name('asa.create');
+Route::post('/asa/transaction/fields', [AssetController::class, 'getTransactionFields']);
+Route::post('/asa/transaction', [AssetController::class, 'store']);
+
+Route::get('/wallet', [WalletController::class, 'index'])->name('wallet.index');
+Route::get('/wallet/install', [WalletController::class, 'create'])->name('wallet.install');
+
+Route::post('/signin', [WalletController::class, 'signIn'])->name('wallet.signin');
+Route::get('/logout', [WalletController::class, 'logout'])->name('wallet.logout');
+
+
