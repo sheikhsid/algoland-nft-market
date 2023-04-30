a3349c376dbc43cad3d60a944fafe5ed020f73b8
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-04-30 20:39:30 +0200

    collection fixed

--- config/algorand.php
+++ config/algorand.php
@@ -21,9 +21,10 @@ return [
     |
     */
     'indexer' => [
-        'api_url' => 'http://node.testnet.algoexplorerapi.io/',
+        'api_url' => 'https://algoindexer.testnet.algoexplorerapi.io/',
         'api_key' => 'ORNfISB9lu6bN9AbdvXA5518L4BBXcVO795LYihb',
         'api_token_header' => 'X-API-Key',
+        'ipfs_url' => 'http://localhost:5001',
     ],
 ];
 
